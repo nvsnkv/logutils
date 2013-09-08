027ede92ed45d73c1270eff2ceeecbd173947ebf
Nikita V Sen'kov <nvsnkv@gmail.com>
2013-09-08 19:16:40 +0400

    Added installation logic executable has been renamed

--- NV.LogUtils/MSBuild/NV.LogUtils.MSBuild.WarningsCounter/Program.cs
+++ NV.LogUtils/MSBuild/NV.LogUtils.MSBuild.WarningsCounter/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using NV.LogUtils.Common;
 using NV.LogUtils.Common.Analyzers;
@@ -19,6 +20,7 @@ namespace NV.LogUtils.MSBuild.WarningsCounter
         private static IReporter _reporter;
         private static string _logFile;
         private static bool _waitKeyPressed;
+        private static bool _install;
 
         public const decimal MaxExpceptionDepth = 3;
 
@@ -27,17 +29,25 @@ namespace NV.LogUtils.MSBuild.WarningsCounter
             try
             {
                 Setup(args);
-                Validate();
 
-                IList<IEntry> entries;
-
-                using (var log = new LogStreamReader(_logFile))
+                if (_install)
                 {
-                    entries = _processor.GetEntries(log);
+                    Install();
                 }
+                else
+                {
+                    Validate();
 
-                var report = _analyzer.Analyze(entries);
-                Console.WriteLine(_reporter.PrintReport(report));
+                    IList<IEntry> entries;
+
+                    using (var log = new LogStreamReader(_logFile))
+                    {
+                        entries = _processor.GetEntries(log);
+                    }
+
+                    var report = _analyzer.Analyze(entries);
+                    Console.WriteLine(_reporter.PrintReport(report));
+                }
             } catch (Exception e)
             {
                 PrintException(e);
@@ -53,14 +63,26 @@ namespace NV.LogUtils.MSBuild.WarningsCounter
 
         }
 
+        private static void Install()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var path = assembly.Location;
+            Environment.SetEnvironmentVariable("WarnCounterPath", path, EnvironmentVariableTarget.User);
+
+            Console.WriteLine("Installation completed.");
+            Console.WriteLine("You should logoff and logon now");
+
+        }
+
         private static void Validate ( )
         {
-            if (string.IsNullOrEmpty(_logFile))
+            if (!_install && string.IsNullOrEmpty(_logFile))
                 throw new ArgumentException("No input file specified!");
         }
 
         private static void Setup ( string[] args )
         {
+            _install = args.Any(x => x == "/install");
             _waitKeyPressed = args.Any(x => (x == "/i") || (x == "/interactive"));
 
             _processor = new MSBuildLogProcessor();
